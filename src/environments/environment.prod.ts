a86da82b1f7c1a0de18c56eb170c05606432bd51
Jin Tak <jkt0904@p2es.com>
2018-04-27 10:17:35 -0600

    basic template and routes laid out. About to deploy to firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyACFExPGM8hXT562kXMY4KWpmraAV2Kb3w',
+    authDomain: 'oshop-5b611.firebaseapp.com',
+    databaseURL: 'https://oshop-5b611.firebaseio.com',
+    projectId: 'oshop-5b611',
+    storageBucket: 'oshop-5b611.appspot.com',
+    messagingSenderId: '735587347886'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -4,5 +4,13 @@
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyACFExPGM8hXT562kXMY4KWpmraAV2Kb3w',
+    authDomain: 'oshop-5b611.firebaseapp.com',
+    databaseURL: 'https://oshop-5b611.firebaseio.com',
+    projectId: 'oshop-5b611',
+    storageBucket: 'oshop-5b611.appspot.com',
+    messagingSenderId: '735587347886'
+  }
 };
